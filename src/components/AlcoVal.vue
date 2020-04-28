36a754142c8bcd4569519d89d3337329bf5902d6
dawsoncanby <dawsoncanby@gmail.com>
2020-04-28 15:20:28 -0600

    av

---
+++ src/components/AlcoVal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>AlcoVal</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
